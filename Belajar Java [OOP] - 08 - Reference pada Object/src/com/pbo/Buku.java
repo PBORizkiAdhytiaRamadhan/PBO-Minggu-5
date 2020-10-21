135cd7ad186539b388b20e701450068ed1e5013a
PBORizkiAdhytiaRamadhan <72726458+PBORizkiAdhytiaRamadhan@users.noreply.github.com>
2020-10-21 21:35:52 +0700

    Add files via upload

---
+++ Belajar Java [OOP] - 08 - Reference pada Object/src/com/pbo/Buku.java
@@ -0,0 +1,16 @@
+package com.pbo;
+
+class Buku{
+    String judul;
+    String penulis;
+
+    Buku(String judul, String penulis){
+        this.judul = judul;
+        this.penulis = penulis;
+    }
+
+    void display(){
+        System.out.println("\nJudul\t: " + this.judul);
+        System.out.println("Penulis\t: " + this.penulis);
+    }
+}
